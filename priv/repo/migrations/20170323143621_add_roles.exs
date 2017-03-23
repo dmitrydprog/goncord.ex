9e0d328673a3b8429500f3977a01a925ecd79de3
Dmitry Dubina <dmitryd.prog@gmail.com>
2017-03-23 15:26:43 +0000

    Added migrations.

---
+++ priv/repo/migrations/20170323143621_add_roles.exs
@@ -0,0 +1,13 @@
+defmodule Goncord.Repo.Migrations.AddRoles do
+  use Ecto.Migration
+
+  def change do
+    create table(:roles) do
+      add :name, :string
+
+      timestamps()
+    end
+
+    create unique_index(:roles, :name)
+  end
+end
