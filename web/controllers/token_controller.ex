339113cc746bc138e5ac4c293d912f5366427965
Dmitry Dubina <dmitryd.prog@gmail.com>
2017-03-23 13:15:00 +0000

    Added logout, validate view. Added unauthenticated hook.

--- web/controllers/token_controller.ex
+++ web/controllers/token_controller.ex
@@ -32,4 +32,31 @@ defmodule Goncord.TokenController do
         |> render("error.json", message: "Проверьте правильность логина, email или пароля")
     end
   end
+
+  def delete(conn, _params) do
+    jwt = Guardian.Plug.current_token(conn)
+    {:ok, claims} = Guardian.Plug.claims(conn)
+
+    case Guardian.revoke!(jwt, claims, nil) do
+      :ok -> send_resp(conn, :no_content, "")
+      {:error, reason} ->
+        Logger.error("Error in #{__MODULE__}.delete. Tried to revoke JWT: #{inspect(reason)}")
+        send_resp(conn, :internal_server_error, "")
+    end
+  end
+
+  def unauthenticated(conn, _params) do
+    conn
+    |> put_status(:unauthorized)
+    |> put_view(Goncord.TokenView)
+    |> render("error.json", message: "Требуется аутентификация")
+  end
+
+  def validate(conn, _params) do
+    user = Guardian.Plug.current_resource(conn)
+    conn
+    |> put_status(:ok)
+    |> put_view(Goncord.UserView)
+    |> render("user.json", user: user)
+  end
 end
