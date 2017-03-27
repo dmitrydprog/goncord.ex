b6e00893afff9566dcbe99254fdc504cea128d58
Dmitry Dubina <dmitryd.prog@gmail.com>
2017-03-27 14:59:02 +0000

    Added menu view.

---
+++ web/views/menu_view.ex
@@ -0,0 +1,11 @@
+defmodule Goncord.MenuView do
+  use Goncord.Web, :view
+
+  def render("menu.json", %{urls: urls}) do
+    render_many(urls, Goncord.MenuView, "url.json")
+  end
+
+  def render("url.json", %{menu: resource}) do
+    %{url: resource.url, name: resource.name}
+  end
+end
