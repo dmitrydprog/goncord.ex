5e03f16f197e91af7eaba38f3911fcaebff1a4de
Dmitry Dubina <dmitryd.prog@gmail.com>
2017-03-27 15:01:15 +0000

    Added create function. Added get_or_create function.

--- web/models/role.ex
+++ web/models/role.ex
@@ -2,11 +2,13 @@ defmodule Goncord.Role do
   @moduledoc false
 
   use Goncord.Web, :model
+  alias Goncord.{Role, Repo}
 
   schema "roles" do
     field :name, :string
 
     many_to_many :users, Goncord.User, join_through: "users_roles", on_delete: :delete_all
+    many_to_many :resources, Goncord.Resource, join_through: "roles_resources", on_delete: :delete_all
 
     timestamps()
   end
@@ -16,5 +18,17 @@ defmodule Goncord.Role do
     |> cast(params, [:name])
     |> unique_constraint(:name)
     |> validate_required([:name])
+  end
+
+  def create(params) do
+    changeset = changeset(%Role{}, params)
+    Repo.insert!(changeset)
+  end
+
+  def get_or_create(params) do
+    query = from r in Goncord.Role,
+        where: r.name == ^params.name
+
+    Repo.one(query) || create(params)
   end
 end
