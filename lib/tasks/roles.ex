532a39cf638e4f10012de1b6b663c9d5e907f09d
Dmitry Dubina <dmitryd.prog@gmail.com>
2017-03-27 15:01:28 +0000

    Refactor code.

--- lib/tasks/roles.ex
+++ lib/tasks/roles.ex
@@ -4,15 +4,8 @@ defmodule Mix.Tasks.AddRoles do
   alias Goncord.{Role, Repo}
 
   def run(roles) do
-    ensure_started(Goncord.Repo, [])
+    ensure_started(Repo, [])
 
-    Enum.each(roles, &{add_role(&1)})
-  end
-
-  defp add_role(role) do
-    changeset = Role.changeset(%Role{}, %{name: role})
-    role = Repo.insert!(changeset)
-
-    IO.inspect(role)
+    Enum.each(roles, &{Role.get_or_create(%{name: &1})})
   end
 end
