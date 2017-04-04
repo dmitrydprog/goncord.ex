ac507fbeceb3a46112718c1f449f07edc6d8cb41
Dmitry Dubina <dmitryd.prog@gmail.com>
2017-04-04 13:04:34 +0000

    Added delete role function.

--- web/controllers/role_controller.ex
+++ web/controllers/role_controller.ex
@@ -3,21 +3,6 @@ defmodule Goncord.RoleController do
 
   def add_role(conn, %{"roles" => roles}) do
     user = Guardian.Plug.current_resource(conn)
-    resource = conn.assigns[:resource]
-
-    if is_nil(resource) do
-      conn
-      |> put_status(:not_found)
-      |> put_view(Goncord.TokenView)
-      |> render("error.json", message: "Ресурс не существует.")
-    end
-
-    unless resource.is_super do
-      conn
-      |> put_status(:not_found)
-      |> put_view(Goncord.TokenView)
-      |> render("error.json", message: "У ресурса недостаточно прав.")
-    end
 
     roles = Enum.map(roles, fn role ->
       Goncord.Role.get_or_create(%{name: role})
@@ -29,5 +14,22 @@ defmodule Goncord.RoleController do
     |> put_status(:ok)
     |> put_view(Goncord.RoleView)
     |> render("index.json", %{roles: roles})
+  end
+
+  def delete_role(conn, %{"roles" => roles}) do
+    user = Guardian.Plug.current_resource(conn)
+    related_roles = Goncord.User.get_all_related_roles(user)
+                    |> Enum.map fn role -> role.name end
+
+    new_roles = related_roles -- roles
+                |> Enum.map fn role -> %{name: role} end
+
+    new_roles = new_roles
+                |> Enum.map fn role -> Goncord.Role.get_or_create role end
+
+    Goncord.Role.set_roles(user, new_roles)
+
+    conn
+    |> send_resp(:no_content, "")
   end
 end
