4886864a2495a44051fddf1c305f287fb5b07a7d
Dmitry Dubina <dmitryd.prog@gmail.com>
2017-04-07 12:10:08 +0000

    Added prod settings.

--- config/prod.exs
+++ config/prod.exs
@@ -5,4 +5,14 @@ config :goncord, Goncord.Endpoint,
 
 config :logger, level: :info
 
+# Configure your database
+config :goncord, Goncord.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "goncord_ex",
+  password: "oi4CheiD",
+  database: "goncord",
+  hostname: "postgres",
+  pool_size: 10
+
+
 import_config "prod.secret.exs"
